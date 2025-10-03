9d4a240683dc169f2977ded306206b52be6da560
fithacker97 <fithacker97@gmail.com>
2025-10-04 00:43:36 +0530

    working on portolio

--- js/cmatrix.js
+++ js/cmatrix.js
@@ -59,7 +59,7 @@ function startMatrix(canvasId) {
     ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = "#0051ffff";
+    ctx.fillStyle = "#0051ff39";
     ctx.font = `${fontSize}px monospace`;
 
     for (let i = 0; i < drops.length; i++) {
