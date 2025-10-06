076e7a3ee7a081b55659b92c227e7859f1c7371e
fithacker97 <fithacker97@gmail.com>
2025-10-06 17:20:43 +0530

    working on portfolio

--- js/typing.js
+++ js/typing.js
@@ -1,30 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const typSpans = document.querySelectorAll(".typ");
-
-    typSpans.forEach(typSpan => {
-        const text = typSpan.textContent;
-        let i = 0;
-
-        function typeWriter() {
-            if (i < text.length) {
-                typSpan.textContent += text.charAt(i);
-                i++;
-                setTimeout(typeWriter, 100); // Typing speed
-            } else {
-                setTimeout(() => {
-                    typSpan.textContent = "";
-                    i = 0;
-                    typeWriter();
-                }, 5000); // Wait 5 seconds before repeating
-            }
-        }
-
-        typSpan.textContent = "";
-        typeWriter();
-    });
-});
-
-
 
 const counters = document.querySelectorAll('.count');
 
